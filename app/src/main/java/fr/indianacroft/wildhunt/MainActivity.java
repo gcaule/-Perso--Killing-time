7270174352a7c450f34ce3980170e71f6256131b
GuillaumeDgr <dangerguillaume@gmail.com>
2017-09-26 15:49:28 +0200

    Commit Home Game Master version 1

--- app/src/main/java/fr/indianacroft/wildhunt/MainActivity.java
+++ app/src/main/java/fr/indianacroft/wildhunt/MainActivity.java
@@ -1,13 +1,33 @@
 package fr.indianacroft.wildhunt;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button butPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hide ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //Button Play
+        butPlay = (Button) findViewById(R.id.butPlay);
+        butPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Tab_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
