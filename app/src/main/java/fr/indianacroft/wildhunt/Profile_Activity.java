bf8bca730e7ddee7631267e0cf019eaa0fc46b12
GuillaumeDgr <dangerguillaume@gmail.com>
2017-09-28 17:43:23 +0200

    Commit Home Game Master version 4

--- app/src/main/java/fr/indianacroft/wildhunt/Profile_Activity.java
+++ app/src/main/java/fr/indianacroft/wildhunt/Profile_Activity.java
@@ -1,34 +1,25 @@
 package fr.indianacroft.wildhunt;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class Tab_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+public class Profile_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab);
+        setContentView(R.layout.activity_profile);
 
         // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -48,63 +39,15 @@ public class Tab_Activity extends AppCompatActivity implements NavigationView.On
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        // Fragment Adapter
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
         // Avatar
         ImageView imageViewAvatar = (ImageView) findViewById(R.id.imageViewAvatar);
         imageViewAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tab_Activity.this, Profile_Activity.class);
-                startActivity(intent);
+                //TODO: Créer lien page profil
+                Toast.makeText(Profile_Activity.this, "You are already on profile page", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    // Fragments
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    Tab1_Activity tab1 = new Tab1_Activity();
-                    return tab1;
-                case 1:
-                    Tab2_Activity tab2 = new Tab2_Activity();
-                    return tab2;
-                case 2:
-                    Tab3_Activity tab3 = new Tab3_Activity();
-                    return tab3;
-                default:
-                    return null;
-            }
-        }
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Create\nnew quest";
-                case 1:
-                    return "Quests\ncreated";
-                case 2:
-                    return "Validate\nquest";
-            }
-            return null;
-        }
     }
 
     // Drawer Menu
@@ -132,18 +75,17 @@ public class Tab_Activity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
         // TODO : remplacer les toasts par des liens
         if (id == R.id.nav_home) {
-            Toast.makeText(Tab_Activity.this, "Créer lien page Home", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Profile_Activity.this, "Créer lien page Home", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_rules) {
-            Toast.makeText(Tab_Activity.this, "Créer lien page Rules", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Profile_Activity.this, "Créer lien page Rules", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_settings) {
-            Intent intent = new Intent(Tab_Activity.this, Profile_Activity.class);
-            startActivity(intent);
+            Toast.makeText(Profile_Activity.this, "You are already on profile page", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_quests) {
-            Toast.makeText(Tab_Activity.this, "Créer lien page Quests", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Profile_Activity.this, "Créer lien page Quests", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_switch) {
-            Toast.makeText(Tab_Activity.this, "Créer lien page Switch", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Profile_Activity.this, "Créer lien page Switch", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_delete) {
-            Toast.makeText(Tab_Activity.this, "Déco joueur", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Profile_Activity.this, "Déco joueur", Toast.LENGTH_SHORT).show();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -157,16 +99,16 @@ public class Tab_Activity extends AppCompatActivity implements NavigationView.On
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    Toast.makeText(Tab_Activity.this, "Créer lien page Home", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Profile_Activity.this, "Créer lien page Home", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_camera:
-                    Toast.makeText(Tab_Activity.this, "Créer lien page camera", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Profile_Activity.this, "Créer lien page camera", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_switch:
-                    Toast.makeText(Tab_Activity.this, "Créer lien page switch", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Profile_Activity.this, "Créer lien page switch", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_notifications:
-                    Toast.makeText(Tab_Activity.this, "Créer lien page Notifications", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Profile_Activity.this, "Créer lien page Notifications", Toast.LENGTH_SHORT).show();
                     return true;
             }
             return false;
