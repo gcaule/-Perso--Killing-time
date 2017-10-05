160db757b35446a5ea5b0134707f3edf53044350
ALRUTEN <laurentnavarro31@gmail.com>
2017-10-05 17:25:59 +0200

    préparation de la page connexion et implémentation de la bdd et la méthode shared

--- app/src/main/java/fr/indianacroft/wildhunt/ConnexionActivity.java
+++ app/src/main/java/fr/indianacroft/wildhunt/ConnexionActivity.java
@@ -1,68 +1,113 @@
 package fr.indianacroft.wildhunt;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class ConnexionActivity extends AppCompatActivity {
 
-    Button play;
-    Button signin;
-    Button login;
-    EditText pseudo;
-    EditText password;
+    FirebaseDatabase ref;
+    DatabaseReference childRef;
+    EditText user_name;
+    EditText user_password;
+    Button nardin;
+    final String MyPREFERENCES = "MyPrefs" ;
+    final String Name = "NameKey";
+    final String Password = "PasswordKey";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
 
-        //TOAST bouton sign in si il n'y a aucune partie en court.
+        user_name = (EditText) findViewById(R.id.user_name);
+        user_password = (EditText) findViewById(R.id.user_password);
+        nardin = (Button) findViewById(R.id.nardin);
+        ref = FirebaseDatabase.getInstance();
+        childRef = ref.getReference("User");
 
-        play  = (Button) findViewById(R.id.button3);
+        final SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        final String name = sharedpreferences.getString(Name, "");
+        final String password = sharedpreferences.getString(Password, "");
 
-                play.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(ConnexionActivity.this, "NO GAME",Toast.LENGTH_LONG).show();
-
-                    }
-                });
-
-        // Click du bouton sign in permettant d'acceder à la page inscription.
-
-        signin = (Button) findViewById(R.id.button5);
-
-                signin.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(ConnexionActivity.this,InscriptionActivity.class);
-                        startActivity(intent);
-                    }
-                });
-
-        // Champs editText deviennent visible quand on click sur le bouton log in.
-
-        pseudo = (EditText) findViewById(R.id.editText5);
-        password = (EditText) findViewById(R.id.editText4);
-        login = (Button) findViewById(R.id.button4);
-
-                pseudo.setVisibility(View.GONE);
-                password.setVisibility(View.GONE);
-
-                login.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                        public void onClick(View view) {
-                            pseudo.setVisibility(View.VISIBLE);
-                            password.setVisibility(View.VISIBLE);
-                }
-            });
+        if( !name.isEmpty() && !password.isEmpty()) {
+            user_name.setText(name);
+            user_password.setText(password);
         }
 
+
+        nardin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameContent = user_name.getText().toString();
+                String passwordContent = user_password.getText().toString();
+                String userId = ref.getReference("User").push().getKey();
+
+
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString(Name, nameContent);
+                editor.putString(Password, passwordContent);
+                editor.commit();
+
+
+                if(user_name.equals(name) && user_password.equals(password)){
+                        Toast.makeText(ConnexionActivity.this, "Thanks", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(ConnexionActivity.this, Tab_HomeJoueur.class);
+                        startActivity(intent);
+                    }else{
+                    User user = new User(nameContent, passwordContent);
+
+                    user.setUser_name(nameContent);
+                    user.setUser_password(passwordContent);
+
+                    childRef.child(userId).setValue(user);
+
+                    Intent intent = new Intent(ConnexionActivity.this, Tab_HomeJoueur.class);
+                    startActivity(intent);
+
+                }
+
+
+
+
+
+                /*user_name.setText("");
+                user_password.setText("");*/
+
+
+
+
+
+
+            }
+        });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
