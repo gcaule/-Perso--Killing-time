8967b1af259302422cde5916e6ee72ac5334d580
GuillaumeDgr <dangerguillaume@gmail.com>
2017-10-09 09:10:58 +0200

    Commit V2 : dernier commit sprint 2 avec profil

--- app/src/main/java/fr/indianacroft/wildhunt/ProfileActivity.java
+++ app/src/main/java/fr/indianacroft/wildhunt/ProfileActivity.java
@@ -11,7 +11,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;   
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
