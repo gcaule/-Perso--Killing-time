a031f204f19ff5eb9dd29c80c77b3d85e7de3d92
HalgandP <halgand.p@gmail.com>
2017-10-19 09:04:10 +0200

    Merge branch 'Guillaume' of https://github.com/HalgandP/WildHunt into Pierre
    # Conflicts:
    #	app/src/main/java/fr/indianacroft/wildhunt/PlayerActivity_PopUp.java

--- app/src/main/java/fr/indianacroft/wildhunt/PlayerActivity_PopUp.java
+++ app/src/main/java/fr/indianacroft/wildhunt/PlayerActivity_PopUp.java
@@ -141,27 +141,27 @@ public class PlayerActivity_PopUp extends AppCompatActivity {
 
 
 
-//                FirebaseDatabase database = FirebaseDatabase.getInstance();
-//                final DatabaseReference refCreatorId = database.getReference("Quest");
-//                refCreatorId.child(mUserQuest).addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                        Quest quest = dataSnapshot.getValue(Quest.class);
-//                        String user_creatorId = quest.getQuest_creatorId();
-//
-//                        // Test Notifs
-//                        sendNotification(
-//                                user_creatorId, //receiver_id, /*who the notification is meant for*/
-//                                "Chat message from John Duff", /*Message to be displayed on the notification*/
-//                                "New chat message", /*Message title*/
-//                                "chat_view" /*Notification type, You can use this to determine what activities to stack when the receiver clicks on the notification item*/
-//                        );
-//                    }
-//                    @Override
-//                    public void onCancelled(DatabaseError error) {
-//                    }
-//                });
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference refCreatorId = database.getReference("Quest");
+                refCreatorId.child(mUserQuest).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        Quest quest = dataSnapshot.getValue(Quest.class);
+                        String user_creatorId = quest.getQuest_creatorId();
+
+                        // Test Notifs
+                        sendNotification(
+                                user_creatorId, //receiver_id, /*who the notification is meant for*/
+                                "Chat message from John Duff", /*Message to be displayed on the notification*/
+                                "New chat message", /*Message title*/
+                                "chat_view" /*Notification type, You can use this to determine what activities to stack when the receiver clicks on the notification item*/
+                        );
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                    }
+                });
 
 
 
